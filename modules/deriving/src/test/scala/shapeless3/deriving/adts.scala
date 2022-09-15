79b32d955f1ea75072664014d643ba83c9c8c677
Georgi Krastev <joro.kr.21@gmail.com>
2022-09-15 12:01:33 +0200

    Optimize applicative traverse to build a tree with factor 128

--- modules/deriving/src/test/scala/shapeless3/deriving/adts.scala
+++ modules/deriving/src/test/scala/shapeless3/deriving/adts.scala
@@ -16,6 +16,8 @@
 
 package shapeless3.deriving
 
+import scala.deriving.Mirror
+
 // ADTs
 
 object adts:
@@ -87,5 +89,31 @@ object adts:
   sealed trait HkNel[F[_]]
   case class HkCons[F[_]](head: F[Int], tail: HkNel[F]) extends HkNel[F]
   case class HkOne[F[_]](head: F[Int]) extends HkNel[F]
+
+  type Large[A] =
+    A *: A *: A *: A *: A *: A *: A *: A *: A *: A *: A *: A *: A *: A *: A *: A *: A *: A *: A *: A *: A *: A *: A *:
+      A *: A *: A *: A *: A *: A *: A *: A *: A *: A *: A *: A *: A *: A *: A *: A *: A *: A *: A *: A *: A *: A *: A *:
+      A *: A *: A *: A *: A *: A *: A *: A *: A *: A *: A *: A *: A *: A *: A *: A *: A *: A *: A *: A *: A *: A *: A *:
+      A *: A *: A *: A *: A *: A *: A *: A *: A *: A *: A *: A *: A *: A *: A *: A *: A *: A *: A *: A *: A *: A *: A *:
+      A *: A *: A *: A *: A *: A *: A *: A *: A *: A *: A *: A *: A *: A *: A *: A *: A *: A *: A *: A *: A *: A *: A *:
+      A *: A *: A *: A *: A *: A *: A *: A *: A *: A *: A *: A *: A *: A *: A *: A *: A *: A *: A *: A *: A *: A *: A *:
+      A *: A *: A *: A *: EmptyTuple
+
+  given Mirror.Product with
+    type Kind = K1.type
+    type MirroredType[A] = Large[A]
+    type MirroredElemTypes[A] = Large[A]
+    type MirroredElemLabels = Large["x"]
+    type MirroredMonoType =
+      ? *: ? *: ? *: ? *: ? *: ? *: ? *: ? *: ? *: ? *: ? *: ? *: ? *: ? *: ? *: ? *: ? *: ? *: ? *: ? *: ? *: ? *: ? *:
+        ? *: ? *: ? *: ? *: ? *: ? *: ? *: ? *: ? *: ? *: ? *: ? *: ? *: ? *: ? *: ? *: ? *: ? *: ? *: ? *: ? *: ? *:
+        ? *: ? *: ? *: ? *: ? *: ? *: ? *: ? *: ? *: ? *: ? *: ? *: ? *: ? *: ? *: ? *: ? *: ? *: ? *: ? *: ? *: ? *:
+        ? *: ? *: ? *: ? *: ? *: ? *: ? *: ? *: ? *: ? *: ? *: ? *: ? *: ? *: ? *: ? *: ? *: ? *: ? *: ? *: ? *: ? *:
+        ? *: ? *: ? *: ? *: ? *: ? *: ? *: ? *: ? *: ? *: ? *: ? *: ? *: ? *: ? *: ? *: ? *: ? *: ? *: ? *: ? *: ? *:
+        ? *: ? *: ? *: ? *: ? *: ? *: ? *: ? *: ? *: ? *: ? *: ? *: ? *: ? *: ? *: ? *: ? *: ? *: ? *: ? *: ? *: ? *:
+        ? *: ? *: ? *: ? *: ? *: ? *: ? *: ? *: ? *: EmptyTuple
+
+    def fromProduct(p: Product): MirroredMonoType =
+      Tuple.fromProduct(p).asInstanceOf
 
 end adts
